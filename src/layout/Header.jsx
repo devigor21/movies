9e4c745f2867a7526502dfb1b8829fe2c4ad052d
Igor <igor8212@mail.ru>
2021-07-26 17:10:46 +0300

    project complete

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,9 +2,9 @@ function Header() {
   return (
     <nav className="indigo lighten-1">
       <div className="container nav-wrapper">
-        <a href="#!" className="brand-logo">Movies App</a>
+        <a href="/" className="brand-logo">Movies App</a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li><a href="https://github.com/devigor21/movies">Repo</a></li>
+          <li><a href="https://github.com/devigor21/movies" target="_blank" rel="noreferrer">Repo</a></li>
         </ul>
       </div>
     </nav>
